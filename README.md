2d83c47181906829d59c1327c9ebc5875150a3e6
Owanikin <oderindeife@gmail.com>
2022-09-06 17:43:22 +0100

    form input

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 1. HTML / JAVASCRIPT for fundme contract
-2. Later on, we will use Nextjs / Reactjs
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function connect() {
 }
 
 async function fund() {
-    const ethAmount = "0.1"
+    const ethAmount = document.getElementById("ethAmount").value
     console.log(`Funding with ${ethAmount}...`)
     if (typeof window.ethereum !== "undefined") {
         // provider / connection to the blockchain
